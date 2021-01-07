cba1b9beac7741955e31c5984a561506a86cf276
Rafael Ferreira <64870651+rafaelfgyn@users.noreply.github.com>
2021-01-07 08:19:27 -0300

    Add files via upload

--- spider.js
+++ spider.js
@@ -1,7 +1,6 @@
 var request = require('request')
 var cheerio = require('cheerio')
 const fs = require('fs')
-const { finished } = require('stream')
 path = require('path')
 
 request(`https://www.imdb.com/chart/moviemeter?sort=ir,desc&mode=simple&page=1`, function (err, res, body) {
@@ -16,27 +15,3 @@ request(`https://www.imdb.com/chart/moviemeter?sort=ir,desc&mode=simple&page=1`,
                     fs.appendFileSync('./Filmes.txt', content + ': ' + rate + '\n')
                 })
             })
-
-// function getImages(uri) {
-//     request(uri, function(error, response, body) {
-//         if(!error && response.statusCode == 200) {
-//             $ = cheerio.load(body)
-//             imgs = $('.posterColumn a img').toArray()
-//             console.log("Downloading...")
-
-//             imgs.forEach(function (img) {
-//             process.stdout.write(".")
-//             img_url = img.attribs.src
-//             if(/^https?:\/\//.test(img_url)) {
-//                 img_name = path.basename(img.attribs.alt + '.jpg').replace(':', '')
-//                 console.log(img_name)
-
-//                 request(img_url).pipe(fs.createWriteStream(`./imagens2/${img_name}`))
-//                 }
-//             })
-//             console.log("Done!")
-//         }
-//     })
-// }
-
-// getImages('https://www.imdb.com/chart/moviemeter?sort=ir,desc&mode=simple&page=1')
